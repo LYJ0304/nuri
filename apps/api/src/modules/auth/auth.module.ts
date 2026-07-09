48e7e3a087eaafe3217e8f1ecdf9e0dfa20fed54
LYJ0304 <omlmg0304@naver.com>
2026-07-09 11:00:11 +0000

    fix(auth): modify expiredIn's type

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,37 +1,46 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import {
+  JwtModule,
+  type JwtSignOptions,
+} from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+
 import { UsersModule } from '../users/users.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { JwtStrategy } from '../strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-  @Module({
-    imports: [
-      UsersModule,
-      PassportModule,
-      JwtModule.registerAsync({
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const expiresIn =
+          configService.get<string>('JWT_ACCESS_EXPIRES_IN') ?? '15m';
+
+        return {
           secret: configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
           signOptions: {
-            expiresIn:
-              configService.get<string>('JWT_ACCESS_EXPIRES_IN') ?? '15m',
+            expiresIn: expiresIn as JwtSignOptions['expiresIn'],
           },
-        }),
-      }),
-    ],
-    controllers: [AuthController],
-    providers: [
-      AuthService,
-      JwtStrategy,
-      JwtAuthGuard,
-    ],
-    exports: [
-      AuthService,
-      JwtAuthGuard,
-    ],
-  })
-  export class AuthModule {}
+        };
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    JwtAuthGuard,
+  ],
+  exports: [
+    AuthService,
+    JwtAuthGuard,
+  ],
+})
+export class AuthModule {}
+
